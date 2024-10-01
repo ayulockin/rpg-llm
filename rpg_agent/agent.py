5d0096744e26787c269cf2d64307546bffbfd5d3
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-10-01 20:31:51 +0530

    add: LLMPredictor

--- rpg_agent/agent.py
+++ rpg_agent/agent.py
@@ -1,10 +1,13 @@
 import weave
 from PIL import ImageGrab
 
+from .llm_predictor import LLMPredictor
+
 
 class DivinityAgent(weave.Model):
+    frame_predictor: LLMPredictor
     game_window_size: tuple[int, int] = (2560, 1440)
-    
+
     @weave.op()
     def get_game_window(self):
         return ImageGrab.grab(
@@ -13,4 +16,10 @@ class DivinityAgent(weave.Model):
 
     @weave.op()
     def predict(self):
-        return self.get_game_window()
+        return {
+            "game_frame": self.get_game_window(),
+            "prediction": self.frame_predictor.predict(
+                system_prompt="You are a helpful assistant meant to describe current frame in detail.",
+                user_prompts=[self.get_game_window()],
+            ),
+        }
