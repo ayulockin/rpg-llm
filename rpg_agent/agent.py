70bd95beb44abec1baaf4b25be57715099481c72
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-11-14 18:06:21 +0530

    update: BaseAgent

--- rpg_agent/agent.py
+++ rpg_agent/agent.py
@@ -18,9 +18,11 @@ class BaseAgent(weave.Model):
     frame_predictor: LLMPredictor
     monitor_index: int = 2
     prompt: str = """
-    We are playing Divinity: Original Sin 2 game. You are provided the current frame of the game. The character in the center of the screen is the player. 
-    
-    We are traying to play this game autonomously thus we need to know what the current state of the game is. Your task is to describe the frame in great detail. Use bullet points and be very specific. Tell the count of the object of interest where necessary and the position of it with respect to the player.
+We are playing Divinity: Original Sin 2 game. You are provided the current frame of the game.
+The character in the center of the screen is the player. 
+We are traying to play this game autonomously thus we need to know what the current state of the game is.
+Your task is to describe the frame in great detail. Use bullet points and be very specific.
+Tell the count of the object of interest where necessary and the position of it with respect to the player.
 """
 
     @weave.op()
